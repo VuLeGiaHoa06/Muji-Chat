dc736daa32af648d689fe20ca908fa66c149d499
Gia Hoa <vulegiahoa06@gmail.com>
2026-04-17 15:23:50 +0700

    update axios

--- front-end/lib/axios.ts
+++ front-end/lib/axios.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from "@/stores/useAuthStore";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
   withCredentials: true, // browser cho phep req dinh kem cookie cho server
 });
 
