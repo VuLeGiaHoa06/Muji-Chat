127f71f2bc62df83da072b849176900c873cea84
Gia Hoa <vulegiahoa06@gmail.com>
2026-04-17 14:08:34 +0700

    add middleware

---
+++ front-end/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = (req: NextRequest) => {
+  console.log("middleware running");
+
+  const token = req.cookies.get("refreshToken")?.value;
+
+  console.log({ token });
+
+  const { pathname } = req.nextUrl;
+  const protectedRoute = ["/"];
+
+  if (protectedRoute.some((route) => pathname.includes(route) && !token)) {
+    return NextResponse.redirect(new URL("/login", req.url)); // http://localhost:3000/login
+  }
+
+  if (pathname.startsWith("/login") && token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|public).*)"],
+};
--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://muji-chat-1-front-end.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
